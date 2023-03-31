d08a0c6199cd38e88e95404f42ee94edb2815dae
Tomaž Ravljen <tomaz.ravljen@gmail.com>
2023-04-01 01:13:08 +0200

    first update with the 2022 unity that is used in the project & test out 100+ units

--- Assets/AniInstancing/Example/Scripts/showFPS.cs
+++ Assets/AniInstancing/Example/Scripts/showFPS.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+//using UnityEngine.UI;
 
 public class showFPS : MonoBehaviour {
 
-    private Text m_text;
+    //private Text m_text;
 	// Use this for initialization
 	void Start () {
-        m_text = GetComponent<Text>();
+        //m_text = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float fps = 1.0f / UnityEngine.Time.smoothDeltaTime;
+        //float fps = 1.0f / UnityEngine.Time.smoothDeltaTime;
         //m_fps = string.Format("FPS: {0}", 1.0f / UnityEngine.Time.smoothDeltaTime);
         //GUI.Label(m_uiPosition, "FPS: " + fps.ToString());
-        m_text.text = "FPS: " + fps.ToString();
+        //m_text.text = "FPS: " + fps.ToString();
     }
 }
